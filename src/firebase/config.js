d4e146f619332f9466c7f24d756ca4784a2add52
MilosNj <njegovanovicm@yahoo.com>
2022-03-18 23:41:47 +0100

    Added firebase-config json

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import * as fbConfig from "./firebase-config.json";
 
-const firebaseConfig = {
-  // Hello. Please look over the README for full instructions for setting up a chat server.
-};
+const firebaseConfig = fbConfig;
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const auth = getAuth();
+const auth = getAuth(app);
 
 export { db, auth };
